bc00498184dda1fd946a09752075488835e53d59
Katlin Anderson <katlin.nbanderson@gmail.com>
2019-09-17 14:51:55 -0700

    add functions to render a list of search results

--- src/App.js
+++ src/App.js
@@ -1,14 +1,68 @@
-import React from "react";
+import React, { Component } from "react"
+import { findRepos } from "./Api"
+import "./App.css"
 
-import './App.css';
+class SearchForm extends Component {
+    constructor() {
+        super()
+        this.state = {
+            resultsArray: null,
+            value: "",
+        }
 
-function SearchForm () {
-  return (
-    <form>
-      <h1>Starstruck Search</h1>
-      <input className="searchInput" name="search" type="text" />
-    </form>
-  );
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange = event => {
+        this.setState({ value: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        findRepos(this.state.value).then(
+            result => {
+                this.setState({ resultsArray: result })
+            },
+            error => {
+                console.log(error)
+            }
+        )
+    }
+
+    renderList = () => {
+        const list = [...this.state.resultsArray].filter(
+            repo => repo.stargazers_count > 127
+        )
+        return list.length ? (
+            <ul>
+                {list.map(repo => (
+                    <li>
+                        <a href={repo.clone_url}>{repo.name}</a>
+                    </li>
+                ))}
+            </ul>
+        ) : (
+            <p>No results found :(</p>
+        )
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <h1>Starstruck Search</h1>
+                    <input
+                        className="searchInput"
+                        name="search"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <button type="submit">Search!</button>
+                </form>
+                {this.state.resultsArray && this.renderList()}
+            </div>
+        )
+    }
 }
 
-export default SearchForm;
+export default SearchForm
