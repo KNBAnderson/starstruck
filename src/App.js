969591f6d00f10cf4e48d83ebc42ecccf1464b93
Katlin Anderson <katlin.nbanderson@gmail.com>
2019-09-17 15:20:04 -0700

    add comments for clarity

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import { findRepos } from "./Api"
 import "./App.css"
 
+//I would usually have the results in a seperate component, but chose to keep it all together for expediency during the interview
 class SearchForm extends Component {
     constructor() {
         super()
@@ -9,14 +10,14 @@ class SearchForm extends Component {
             resultsArray: null,
             value: "",
         }
-
         this.handleSubmit = this.handleSubmit.bind(this)
     }
-
+    //This collects the value in the text input and updates it with every change made
     handleChange = event => {
         this.setState({ value: event.target.value })
     }
 
+    //This will trigger when the submit buttonis clicked. It will call the promise made from the findRepos function, and use .then to resolve it by setting the returned data to state.
     handleSubmit = event => {
         event.preventDefault()
         findRepos(this.state.value).then(
@@ -30,18 +31,20 @@ class SearchForm extends Component {
     }
 
     renderList = () => {
+      //This will filter out all results in the resultsArray that represent repos with less than 127 stars
         const list = [...this.state.resultsArray].filter(
-            repo => repo.stargazers_count > 127
+            repo => repo.stargazers_count >= 127
         )
         return list.length ? (
-            <ul>
+          <ul>
                 {list.map(repo => (
-                    <li>
+                  <li>
                         <a href={repo.clone_url}>{repo.name}</a>
                     </li>
                 ))}
             </ul>
         ) : (
+          // If the nothing is found to meet all the requirements it will render differently
             <p>No results found :(</p>
         )
     }
@@ -59,6 +62,7 @@ class SearchForm extends Component {
                     <br />
                     <button type="submit">Search!</button>
                 </form>
+                {/* This short circuit will act as a conditional and only the returned value of renderList once resultArray has a truthy value in it */}
                 {this.state.resultsArray && this.renderList()}
             </div>
         )
